b1a06c5cf787b6ab5c0c910d5e37f2ba67196ae0
K-Kavitha-Kiran <kavitha.kasivajjhala@gmail.com>
2021-06-22 13:57:26 +0530

    Optimised Code

--- main.py
+++ main.py
@@ -9,13 +9,85 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
-def sendmail():
+def sendmail(text, html, data, subject, header):
+
     me = 'freezls.project@gmail.com'
     password = "MiniProject"
     server = 'smtp.gmail.com:587'
     #you = input()
     you = 'kavitha.kasivajjhala@gmail.com'
 
+    text = text.format(table=tabulate(data, headers=header, tablefmt="grid"))
+    html = html.format(table=tabulate(data, headers=header, tablefmt="html"))
+
+    message = MIMEMultipart(
+        "alternative", None, [MIMEText(text), MIMEText(html,'html')])
+
+    message['Subject'] = subject
+    message['From'] = me
+    message['To'] = you
+    server = smtplib.SMTP(server)
+    server.ehlo()
+    server.starttls()
+    server.login(me, password)
+    server.sendmail(me, you, message.as_string())
+    server.quit()
+
+def alert(ldata):
+    text = """
+    Hello, User.
+    Here is the list of Items about to expire:
+    {table}
+    Regards,
+    Smart Fridge"""
+
+    html = """
+    <html><body><p>Hello, User.</p>
+    <p>Here is the list of Items about to expire:</p>
+    {table}
+    <p>Regards,</p>
+    <p>Smart Fridge</p>
+    </body></html>
+    """
+
+    with open('items_list.csv') as input_file:
+        reader = csv.reader(input_file)
+        ldata = list(reader)
+    #alert prior to expiry
+    #alertdays=2
+    #sorting in order of expiry
+    ldata=(sorted(ldata, key = lambda x: x[2]))
+    data=[]
+    for i in range(len(ldata)):
+        #if(data[i][2]-date.today()>alertdays):
+            #break
+        data.append([ldata[i][0],ldata[i][2]])
+
+    if(len(data)>=1):
+        subject= "Alert!! Items about to Expire"
+        header = ["Item Name","Date of Expiry"]
+        sendmail(text, html, data, subject, header)
+        print("=>  Alert Sent  <=")
+        #prevdate=date.today()
+    else:
+        print("Your Fridge is Fresh!")
+
+#dictionary to store Category, Best Before.
+items = {}
+items["banana"] = ["Fruit", 5]
+items["apple"] = ["Fruit", 9]
+items["carrot"] = ["Vegetable", 11]
+items["orange"] = ["Fruit", 14]
+items["broccoli"] = ["Vegetable", 4]
+items["tomato"] = ["Vegetable", 3]
+items["bottle"] = ["Other", 5]
+items["sandwich"] = ["Snacks", 3]
+items["hot dog"] = ["Snacks", 7]
+items["pizza"] = ["Snacks", 3]
+items["donut"] = ["Snacks", 6]
+items["cake"] = ["Snacks", 3]
+
+def listMail():
     text = """
     Hello, User.
     Here is your data:
@@ -35,181 +107,95 @@ def sendmail():
     with open('items_list.csv') as input_file:
         reader = csv.reader(input_file)
         data = list(reader)
+    header = ["Name", "Date of Insertion", "Expiry Date", "Category"]
+    subject = "Here is your data"
+    sendmail(text, html, data, subject, header)
+    print("=>  List Mailed  <=")
+   
+def recognition():
+    thres = 0.70 # Threshold to detect object
 
-    #sorting in order of expiry
-    data=(sorted(data, key = lambda x: x[2]))
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    # cap.set(3,1280)
+    # cap.set(4,720)
+    # cap.set(10,70)
+    # print(cap)
+    classNames= []
+    classFile = 'coco.names'
+    with open(classFile,'rt') as f:
+        classNames = f.read().rstrip(' ').split('\n')
+    # print(classNames)
+    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath = 'frozen_inference_graph.pb'
 
-    text = text.format(table=tabulate(data, headers="firstrow", tablefmt="grid"))
-    html = html.format(table=tabulate(data, headers="firstrow", tablefmt="html"))
+    net = cv2.dnn_DetectionModel(weightsPath,configPath)
+    net.setInputSize(320,320)
+    net.setInputScale(1.0/ 127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
+    if(True):
+        i = 1
+        flag = False
+        while(True):
+            success,img = cap.read()
+            # cv2.imshow('',img)
+            classIds, confs, bbox = net.detect(img,confThreshold=thres)
+                # print(classIds,bbox)
 
-    message = MIMEMultipart(
-        "alternative", None, [MIMEText(text), MIMEText(html,'html')])
-
-    message['Subject'] = "Items in your fridge"
-    message['From'] = me
-    message['To'] = you
-    server = smtplib.SMTP(server)
-    server.ehlo()
-    server.starttls()
-    server.login(me, password)
-    server.sendmail(me, you, message.as_string())
-    print("=>  Mail Sent  <=")
-    server.quit()
+            if len(classIds) != 0:
+                for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    # cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),
+                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    # cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    if(classNames[classId-1] in items):
+                        return classNames[classId-1]
+            cv2.imshow('Output',img)
+            cv2.waitKey(1)
 
 def insert():
     #recognition
-    thres = 0.70 # Threshold to detect object
-
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    # cap.set(3,1280)
-    # cap.set(4,720)
-    # cap.set(10,70)
-    # print(cap)
-    classNames= []
-    classFile = 'coco.names'
-    with open(classFile,'rt') as f:
-        classNames = f.read().rstrip(' ').split('\n')
-    # print(classNames)
-    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-    weightsPath = 'frozen_inference_graph.pb'
-
-    net = cv2.dnn_DetectionModel(weightsPath,configPath)
-    net.setInputSize(320,320)
-    net.setInputScale(1.0/ 127.5)
-    net.setInputMean((127.5, 127.5, 127.5))
-    net.setInputSwapRB(True)
-    if(True):
-        i = 1
-        flag = False
-        while(True):
-            success,img = cap.read()
-            # cv2.imshow('',img)
-            classIds, confs, bbox = net.detect(img,confThreshold=thres)
-                # print(classIds,bbox)
-
-            if len(classIds) != 0:
-                for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                    prev = 'person'
-                    if(classNames[classId-1]!=prev and (classNames[classId-1]=='banana' or classNames[classId-1]=='apple' or classNames[classId-1]=='orange' or classNames[classId-1]=='broccoli' or classNames[classId-1]=='tomato' or classNames[classId-1]=='carrot')):
-                        print("added",classNames[classId-1],"list updated")
-                        prev = classNames[classId-1]
-                        with open('items_list.csv', mode='a',newline='') as items_list:
-                            #to avoid extra line gaps
-                            items_writer = csv.writer(items_list, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                            today = date.today()
-                            dateTimeObj = datetime.now()
-                            day = ""
-                            day += str(dateTimeObj.day)+"-"+str(dateTimeObj.month)+"-"+str(dateTimeObj.year)+" "+str(dateTimeObj.hour)+":"+str(dateTimeObj.minute)
-                            # date = datetime.datetime().strftime(today,"%d-%m-%Y, %H:%M")
-                            if(classNames[classId-1]=='banana'):
-                                expiry = today + timedelta(days=4)
-                                items_writer.writerow([classNames[classId-1], day, expiry])
-                            elif(classNames[classId-1]=='apple'):
-                                expiry = today + timedelta(days=6)
-                                items_writer.writerow([classNames[classId-1], day, expiry])
-                            elif(classNames[classId-1]=='carrot'):
-                                expiry = today + timedelta(days=7)
-                                items_writer.writerow([classNames[classId-1], day, expiry])
-                            elif(classNames[classId-1]=='orange'):
-                                expiry = today + timedelta(days=5)
-                                items_writer.writerow([classNames[classId-1], day, expiry])
-                            elif(classNames[classId-1]=='broccoli'):
-                                expiry = today + timedelta(days=3)
-                                items_writer.writerow([classNames[classId-1], day, expiry])    
-                            else:
-                                expiry = today + timedelta(days=4)
-                                items_writer.writerow([classNames[classId-1], day, expiry])
-                            
-                        flag = True
-                        break
-                    # cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),
-                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                    # cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            if(flag == True):
-                break
-            cv2.imshow('Output',img)
-            cv2.waitKey(1)
-
+    Item_name = recognition()
+    print("added",Item_name,"list updated")
+    with open('items_list.csv', mode='a',newline='') as items_list:
+        #to avoid extra line gaps
+        items_writer = csv.writer(items_list, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        dateTimeObj = datetime.now()
+        day = str(str(dateTimeObj.day)+"-"+str(dateTimeObj.month)+"-"+str(dateTimeObj.year)+" "+str(dateTimeObj.hour)+":"+str(dateTimeObj.minute))
+        # date = datetime.datetime().strftime(today,"%d-%m-%Y, %H:%M")
+        expiry = date.today() + timedelta(days=items[Item_name][1])
+        # row of csv file     ( item name        insert datetime            expiry                          Category          )
+        items_writer.writerow([Item_name, day, expiry, items[Item_name][0]])
+            
 def delete():
     #store csv file data into list
+    Item_name = recognition()
     with open('items_list.csv') as input_file:
         reader = csv.reader(input_file)
         data = list(reader)
-    #recognition
-    thres = 0.70 # Threshold to detect object
 
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    # cap.set(3,1280)
-    # cap.set(4,720)
-    # cap.set(10,70)
-    # print(cap)
-    classNames= []
-    classFile = 'coco.names'
-    with open(classFile,'rt') as f:
-        classNames = f.read().rstrip(' ').split('\n')
-    # print(classNames)
-    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-    weightsPath = 'frozen_inference_graph.pb'
+        for idx in range(len(data)):
+            if(data[idx][0]==Item_name):
+                print("deleted",data[idx][0],"inserted on",data[idx][1])
+                data.remove(data[idx])
+                break 
+        #reopen csv file and write changes
+        with open('items_list.csv', 'w', newline='') as writeFile:
+            writer = csv.writer(writeFile)
+            writer.writerows(data)
 
-    net = cv2.dnn_DetectionModel(weightsPath,configPath)
-    net.setInputSize(320,320)
-    net.setInputScale(1.0/ 127.5)
-    net.setInputMean((127.5, 127.5, 127.5))
-    net.setInputSwapRB(True)
-    if(True):
-        i = 1
-        flag = False
-        while(True):
-            success,img = cap.read()
-            # cv2.imshow('',img)
-            classIds, confs, bbox = net.detect(img,confThreshold=thres)
-                # print(classIds,bbox)
-
-            if len(classIds) != 0:
-                for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                    prev = 'person'
-                    if(classNames[classId-1]!=prev and (classNames[classId-1]=='banana' or classNames[classId-1]=='apple' or classNames[classId-1]=='orange' or classNames[classId-1]=='broccoli' or classNames[classId-1]=='tomato' or classNames[classId-1]=='carrot')):
-                        prev = classNames[classId-1]
-                        with open('items_list.csv', mode='a',newline='') as items_list:
-                            items_writer = csv.writer(items_list, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                            today = date.today()
-                            dateTimeObj = datetime.now()
-                            day = ""
-                            day += str(dateTimeObj.day)+"-"+str(dateTimeObj.month)+"-"+str(dateTimeObj.year)+" "+str(dateTimeObj.hour)+":"+str(dateTimeObj.minute)
-                            # date = datetime.datetime().strftime(today,"%d-%m-%Y, %H:%M")
-                            #delete oldest occurance of the item
-                            for idx in range(len(data)):
-                                if(data[idx][0]==classNames[classId-1]):
-                                      print("deleted",data[idx][0],"inserted on",data[idx][1])
-                                      data.remove(data[idx])
-                                      break 
-                            with open('items_list.csv', 'w', newline='') as writeFile:
-                                writer = csv.writer(writeFile)
-                                writer.writerows(data)
-
-                        flag = True
-                        break
-                    # cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),
-                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                    # cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            if(flag == True):
-                break
-            cv2.imshow('Output',img)
-            cv2.waitKey(1)
-
-#main code
-k = input("Enter your choice\n \ti to insert\td to delete\tm to mail\ts to stop\n")
+#input from user
+k = input("Enter your choice\n \ti to insert\tl for list\ta for alert\td to delete\n")
 if(k=='i'):
     while(k=='i'):
         insert()
         k = input("Enter your choice\n\ti to insert\ts to stop\n")
-elif(k=='m'):
-    sendmail()
+elif(k=='l'):
+    listMail()
 elif(k=='d'):
     delete()
+elif(k=='a'):
+    alert()
 else:
-    exit(0)
+    print("NO OPTION, Run again")
